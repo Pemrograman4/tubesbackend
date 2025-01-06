26276e3dff7f2f53e0e800cb219ed4facd19c265
salwaputri123 <salwamutfiaindahputri@gmail.com>
2025-01-06 14:10:20 +0700

    model siswa

--- models/model.go
+++ models/model.go
@@ -16,3 +16,12 @@ type Course struct {
 	Cost        float64 `json:"cost"`
 	Description string  `json:"description"`
 }
+
+type Siswa struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	FullName    string             `bson:"fullname,omitempty" json:"fullname,omitempty"`
+	Address     string             `bson:"address,omitempty" json:"address,omitempty"`
+	PhoneNumber string             `bson:"phonenumber,omitempty" json:"phonenumber,omitempty"`
+	Email       string             `bson:"email,omitempty" json:"email,omitempty"`
+	Status      string             `bson:"status,omitempty" json:"status,omitempty"` // "aktif" or "nonaktif"
+}
